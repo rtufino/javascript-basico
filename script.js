f716c9686aec541d77be8e40a45c51b017d8c697
Rodrigo Tufiño <rtufino@ups.edu.ec>
2025-06-16 15:01:03 -0500

    Agregar item a lista con DOM

--- script.js
+++ script.js
@@ -31,9 +31,25 @@ function arreglos(){
         console.log('No es una pera');
     }
 }
-
+// Agregar evente a un elemento HTML
 document.getElementById("btnSaludar").addEventListener(
     "click", function () {
         console.log("Hola!");
     }
 );
+
+// Modificar el HTML a través del DOM
+document.getElementById("btnAgregar").addEventListener(
+    "click", () => {
+        // Obtener el elemnto "lista" (ul) del documento HTML
+        let lista = document.getElementById("lista");
+        // Creando un nuevo elemento HTML, li = list item
+        let nuevoItem = document.createElement("li");
+        // Obteniendo el total de ítems de la lista
+        let totalItems = lista.childElementCount;
+        // Agregando un contenido (texto) al item
+        nuevoItem.textContent = `Item ${totalItems + 1}`;
+        // Agregar item a la lista
+        lista.appendChild(nuevoItem);
+    }
+)
